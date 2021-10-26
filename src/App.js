1151246437e7ce001533dd51a00b88a59c32964e
xjzsq <sdxjzsq@126.com>
2021-10-27 00:14:38 +0800

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
+
+
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import { Stage, Layer, Rect, Shape, Text } from 'react-konva';
+import Konva from 'konva';
+
+function BaseTriangle() {
+  return (
+    <Shape
+      sceneFunc={(context, shape) => {
+        context.beginPath();
+        context.moveTo(400, 500);
+        context.lineTo(350, 560);
+        context.lineTo(450, 560);
+        context.closePath();
+        // (!) Konva specific method, it is very important
+        context.fillStrokeShape(shape);
+      }}
+      fill="#00BFFF"
+      stroke=""
+      strokeWidth={4}
+      shadowBlur={1}
+    />
+  );
+}
 
 function App() {
+  // Stage is a div container
+  // Layer is actual canvas element (so you may have several canvases in the stage)
+  // And then we have canvas shapes inside the Layer
+
+  // const rectRef = React.useRef();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={()=>{
+        // const node = rectRef.current;
+        const anim = new Konva.Animation(
+          (frame) => {
+          }
+        )
+      }}>
+        Toggle animation
+      </button>
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+          
+          <BaseTriangle />
+          <Shape
+            x={200}
+            y={490}
+            width={400}
+            height={10}
+            fill="#00BFFF"
+            shadowBlur={1}
+          />
+        </Layer>
+      </Stage>
+    </>
   );
 }
 
